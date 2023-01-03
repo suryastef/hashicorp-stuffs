b6259c76e50bca7bb3733c4fc72cb9ca7687d451
Surya Stefanus <suryastef@gmail.com>
2023-01-04 01:49:56 +0700

    initial commit

---
+++ terraform/GCP/enable-service-api/kubernetes-engine-api.tf
@@ -0,0 +1,12 @@
+resource "google_project_service" "project" {
+  project = "sandbox-suryastef"
+  service = "container.googleapis.com"
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+
+  disable_dependent_services = true
+  disable_on_destroy = true
+}
---
+++ terraform/GCP/gcloud-setup/README.md
@@ -0,0 +1,13 @@
+# Setup Gcloud
+
+## Authenticate GCP Project
+
+```
+gcloud auth login
+```
+
+## Switch GCP Project
+
+```
+gcloud config set project $(PROJECT_NAME)
+```
