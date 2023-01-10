e0f3d67b89b956673b49285bcbbb993d487b4cef
Surya Stefanus <suryastef@gmail.com>
2023-01-11 04:06:00 +0700

    feat : add tfstate-gcs-backend

---
+++ terraform/GCP/tfstate-gcs-backend/main.tf
@@ -0,0 +1,22 @@
+# Create a GCS Bucket
+resource "google_storage_bucket" "tf_state" {
+  project                     = var.gcp_project
+  name                        = var.bucket-name
+  location                    = var.gcp_region
+  force_destroy               = false
+  storage_class               = var.storage-class
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = true
+  }
+
+  logging {
+    log_bucket = var.log-bucket-name
+  }
+}
+
+output "gcs_terraform_state_buckets" {
+  description = "The Cloud Storage buckets used for storing terraform states."
+  value       = google_storage_bucket.tf_state.name
+}
---
+++ terraform/GCP/tfstate-gcs-backend/terraform.tfvars
@@ -0,0 +1,8 @@
+# GCP Settings
+gcp_project = "sandbox-suryastef"
+gcp_region  = "asia-southeast2"
+
+# Bucket
+bucket-name     = "suryastef-tfstate"
+storage-class   = "REGIONAL"
+log-bucket-name = "suryastef-tfstate-log"
